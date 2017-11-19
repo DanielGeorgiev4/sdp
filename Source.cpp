13c2d6a96360186855798d458c619ae5a8f8b47c
DanielGeorgiev4 <fellyder@abv.bg>
2017-11-19 15:42:47 +0200

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,23 +1,51 @@
 #include <iostream>
-#include "BinTree.h"
 using namespace std;
-
-bool test(const int& x)
-{
-	return x >= 100;
-}
-
-void test()
-{
-	BinTree<int> t;
-	t.add(10, "");
-	t.add(5, "L");
-	t.add(20, "R");
-	t.add(15, "RL");
-	t.add(25, "RR");
-	cout << t.get_element("RLR") << endl;
-}
 int main()
 {
-	test();
+	int n;
+	cout << "Enter the number of point you are aбout to enter: ";
+	cin >> n;
+
+	//*vectors е динамичен масив, не знам дали сте ги вземали, но така се прави
+	double *vectors = new double[n], x, y, max_vector,buff;
+
+	// това ти е въвежданте на точките
+	for (unsigned i = 0; i < n; i++)
+	{
+		cout << "Enter x" << i + 1 << ": ";
+		cin >> x;
+		cout << "Enter y" << i + 1 << ": ";
+		cin >> y;
+		vectors[i] = sqrt(x*x + y*y);
+	}
+	
+	// това ти намира най-дългия вектор в max_vector
+	max_vector = vectors[0];
+	for (unsigned i = 0; i < n; i++) if (vectors[i] > max_vector) max_vector = vectors[i];
+	
+	// това ти сортира масива то векторите vectors в низходящ ред
+	for (unsigned i = 0; i < n - 1; i++)
+		for (unsigned k = i + 1; k < n; k++)
+		{
+			//ако не ви дава да използвате swap да разменяш числа ползвай това отдолу
+			if (vectors[i] < vectors[k])
+			{
+				buff = vectors[i];
+				vectors[i] = vectors[k];
+				vectors[k] = buff;
+			}
+			// аз бих го направил така, ако ви дава да ползвате swap
+			/*if (vectors[i] < vectors[k]) swap(vectors[i], vectors[k]);*/
+		}
+
+	//тук ги принтя
+	cout << endl << "Longest vector: " << max_vector << endl;
+	for (unsigned i = 0; i < n; i++)
+	{
+		cout << i + 1 << ") " << vectors[i] << endl;
+	}
+
+	//тук трия масива понеже е заделен в hesh-a, но ако не сте го учили може да не го пишеш,
+	//но е грешка да не го пишеш попринцип, ти си прецени
+	delete[] vectors;
 }
